0532fe0f390d1f0a9bd0a0cdaee86d335ae73443
changcc <49990243+changli4313@users.noreply.github.com>
2024-05-17 16:20:17 +0800

    Update ds.js

--- js/ds.js
+++ js/ds.js
@@ -2047,7 +2047,7 @@
                     key: 'genDom',
                     value: function genDom() {
                         this.createDom(_defineProperty({
-                            href: 'https://flyingwzb.github.io/',
+                            href: 'https://qm.qq.com/q/vRThfnDEJy',
                             target: '_blank'
                         }, _dom.textKey, '了解更多⚽🏀💻'), 'a');
                     }
